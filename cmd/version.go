decab90ed7ebad513f883450d8856970e47f9c91
a-was <adam.wasilewski@unix.com.pl>
2022-07-03 20:17:41 +0200

    Create version cmd

---
+++ cmd/version.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of Github Package Manager",
+	Long:  `All software has versions. This is Github Package Manager's`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("You are using Github Package Manager v0.0.1")
+	},
+}
