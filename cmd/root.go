a3d79b81872a89652b67d9f5b0f555cb8363523c
a-was <adam.wasilewski@unix.com.pl>
2022-07-03 20:15:20 +0200

    Create root command

---
+++ cmd/root.go
@@ -0,0 +1,15 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "github-package-manager",
+	Short: "A GitHub package manager",
+}
+
+// Execute executes the root command.
+func Execute() error {
+	return rootCmd.Execute()
+}
