f4bfea90ab110282a478cfe97f223f7fd97b3ac0
a-was <adam.wasilewski@unix.com.pl>
2022-09-01 15:32:55 +0200

    Rewrite uncompressFile

--- cmd/install/archive.go
+++ cmd/install/archive.go
@@ -1,58 +1,66 @@
 package install
 
 import (
+	"fmt"
 	"os/exec"
-	"path/filepath"
+	"strings"
 )
 
 func uncompressFile(baseFolder, filename string) error {
-	ext := filepath.Ext(filename)
-	u := uncompress{
-		filename: filename,
+	var cmd []string
+	var options []string
+	parts := strings.Split(filename, ".")
+	ln := len(parts)
+	switch {
+	case ln >= 3:
+		options = []string{
+			fmt.Sprintf(".%s.%s", parts[ln-2], parts[ln-1]),
+			fmt.Sprintf(".%s", parts[ln-1]),
+		}
+	case ln >= 2:
+		options = []string{
+			fmt.Sprintf(".%s", parts[ln-1]),
+		}
 	}
 
-	switch ext {
-	case ".zip":
-		u.cmd = []string{"unzip"}
-	case ".tar.gz", ".gz", ".tgz", ".tar.xz", ".xz":
-		u.cmd = []string{"tar", "xzf"}
-	// case ".gz":
-	// 	u.cmd = []string{"gunzip"}
-	case ".tar.bz2", ".bz2", ".tbz2":
-		u.cmd = []string{"tar", "xjf"}
-	// case ".bz2":
-	// 	u.cmd = []string{"bunzip2"}
-	case ".tar":
-		u.cmd = []string{"tar", "xf"}
-	case ".Z":
-		u.cmd = []string{"uncompress"}
-	case ".rar":
-		u.cmd = []string{"unrar", "x"}
-	case ".jar":
-		u.cmd = []string{"jar", "-xvf"}
-	default:
-		// not an archive
+	for _, option := range options {
+		switch option {
+		case ".zip":
+			cmd = []string{"unzip"}
+		case ".tar.gz", ".tgz", ".tar.xz", ".xz":
+			cmd = []string{"tar", "xzf"}
+		case ".gz":
+			cmd = []string{"gunzip"}
+		case ".tar.bz2", ".tbz2":
+			cmd = []string{"tar", "xjf"}
+		case ".bz2":
+			cmd = []string{"bunzip2"}
+		case ".tar":
+			cmd = []string{"tar", "xf"}
+		case ".Z":
+			cmd = []string{"uncompress"}
+		case ".rar":
+			cmd = []string{"unrar", "x"}
+		case ".jar":
+			cmd = []string{"jar", "-xvf"}
+		default:
+			continue
+		}
+		break
+	}
+	if cmd == nil {
 		return nil
-		// return errors.New("unsupported file extension")
 	}
+	cmd = append(cmd, filename)
 
-	if err := u.exec(); err != nil {
+	command := exec.Command(cmd[0], cmd[1:]...)
+	command.Dir = repoFolder
+	err := command.Run()
+	// d, err := command.CombinedOutput()
+	// fmt.Println("uncompress:", string(d), err)
+	if err != nil {
 		return err
 	}
+
 	return nil
-}
-
-type uncompress struct {
-	filename string
-	cmd      []string
-}
-
-func (u *uncompress) exec() error {
-	u.cmd = append(u.cmd, u.filename)
-	cmd := exec.Command(u.cmd[0], u.cmd[1:]...)
-	cmd.Dir = repoFolder
-	return cmd.Run()
-	// d, err := cmd.CombinedOutput()
-	// fmt.Println("uncompress:", string(d), err)
-	// return err
 }
