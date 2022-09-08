7f5a2196c31a231e51fa68f719e8a30bb11f97c4
a-was <adam.wasilewski@unix.com.pl>
2022-09-08 12:08:16 +0200

    Change displayed version to v0.1.0

--- cmd/version.go
+++ cmd/version.go
@@ -15,6 +15,6 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of Github Package Manager",
 	Long:  `All software has versions. This is Github Package Manager's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("You are using Github Package Manager v0.0.1")
+		fmt.Println("You are using Github Package Manager v0.1.0")
 	},
 }
